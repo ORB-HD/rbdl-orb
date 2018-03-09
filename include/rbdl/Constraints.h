d8fb7418439534395fc6fc05958070b37e673cbc
Monika Harant <monika.harant@iwr.uni-heidelberg.de>
2018-03-09 09:26:14 +0100

    modified rbdl-python-wrapper, included muscle-wrapper
    --HG--
    branch : dev

--- include/rbdl/Constraints.h
+++ include/rbdl/Constraints.h
@@ -916,7 +916,6 @@ void SolveConstrainedSystemDirect (
   const Math::MatrixNd &G, 
   const Math::VectorNd &c, 
   const Math::VectorNd &gamma, 
-  Math::VectorNd &qddot, 
   Math::VectorNd &lambda, 
   Math::MatrixNd &A, 
   Math::VectorNd &b,
